9ebc4c1e7c6df5fd7f8340df6ff3fb67f327eb12
Favour <ayofavour7@gmail.com>
2020-03-24 20:46:04 +0100

    Revert "Revert "chore: Add redux, redux-saga, firebase [TSK-BE-1]""

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import * as firebaseInit from 'firebase';
 import * as ROUTES from '../src/constants/routes';
 import './App.css';
 
@@ -10,6 +11,22 @@ import Tips from './pages/Tips';
 import Report from './pages/Report';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCOMuidIz53S_1_iW5isfoiy3J66QVp720",
+  authDomain: "dsc-covid-19.firebaseapp.com",
+  databaseURL: "https://dsc-covid-19.firebaseio.com",
+  projectId: "dsc-covid-19",
+  storageBucket: "dsc-covid-19.appspot.com",
+  messagingSenderId: "724650717987",
+  appId: "1:724650717987:web:1e96e61a05f00140a35296",
+  measurementId: "G-LJRVG3223S"
+};
+
+export const firebase = firebaseInit.initializeApp(firebaseConfig);
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
+export const auth = firebase.auth();
 
 const App = () => (
   <Router>
