83eb53b25a26d8aac9dfa256a188ef873bb20a57
joshtom <olajidejoshua4real@gmail.com>
2020-03-25 12:48:46 +0100

    remove firebase config from App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -14,22 +14,6 @@ import Report from './pages/Report';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCOMuidIz53S_1_iW5isfoiy3J66QVp720",
-//   authDomain: "dsc-covid-19.firebaseapp.com",
-//   databaseURL: "https://dsc-covid-19.firebaseio.com",
-//   projectId: "dsc-covid-19",
-//   storageBucket: "dsc-covid-19.appspot.com",
-//   messagingSenderId: "724650717987",
-//   appId: "1:724650717987:web:1e96e61a05f00140a35296",
-//   measurementId: "G-LJRVG3223S"
-// };
-
-// export const firebase = firebaseInit.initializeApp(firebaseConfig);
-// export const firestore = firebase.firestore();
-// export const storage = firebase.storage();
-// export const auth = firebase.auth();
-
 const App = () => (
   <AuthProvider>
   <Router>
--- src/base.js
+++ src/base.js
@@ -12,3 +12,20 @@ const app = firebase.initializeApp({
 });
 
 export default app;
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCOMuidIz53S_1_iW5isfoiy3J66QVp720",
+//   authDomain: "dsc-covid-19.firebaseapp.com",
+//   databaseURL: "https://dsc-covid-19.firebaseio.com",
+//   projectId: "dsc-covid-19",
+//   storageBucket: "dsc-covid-19.appspot.com",
+//   messagingSenderId: "724650717987",
+//   appId: "1:724650717987:web:1e96e61a05f00140a35296",
+//   measurementId: "G-LJRVG3223S"
+// };
+
+// export const firebase = firebaseInit.initializeApp(firebaseConfig);
+// export const firestore = firebase.firestore();
+// export const storage = firebase.storage();
+// export const auth = firebase.auth();
