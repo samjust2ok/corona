8fe3923440697852b034c9b0cc8b6fa384fa98b5
joshtom <olajidejoshua4real@gmail.com>
2020-03-25 13:09:33 +0100

    Update firebase credentials made sure auth is still working

--- src/base.js
+++ src/base.js
@@ -1,8 +1,10 @@
-import * as firebase from "firebase/app";
+import * as firebaseInit from "firebase/app";
 import "firebase/auth";
+import 'firebase/firestore';
+import 'firebase/storage';
 
 
-const app = firebase.initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyCOMuidIz53S_1_iW5isfoiy3J66QVp720",
   authDomain: "dsc-covid-19.firebaseapp.com",
   databaseURL: "https://dsc-covid-19.firebaseio.com",
@@ -11,8 +13,10 @@ const app = firebase.initializeApp({
   messagingSenderId: "724650717987",
   appId: "1:724650717987:web:1e96e61a05f00140a35296",
   measurementId: "G-LJRVG3223S"
-});
+};
 
+
+export const app = firebaseInit.initializeApp(firebaseConfig);
 export const firestore = app.firestore();
 export const storage = app.storage();
 export const auth = app.auth();
