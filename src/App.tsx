60d65569c1d56be6611a6a167de85ef5e2be1ca1
joshtom <olajidejoshua4real@gmail.com>
2020-03-25 12:53:24 +0100

    Firebase config moved to base.js and credentials updated

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import Report from './pages/Report';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 
+
 const App = () => (
   <AuthProvider>
   <Router>
--- src/base.js
+++ src/base.js
@@ -3,29 +3,22 @@ import "firebase/auth";
 
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyA72w9xvDJdfyn_BB5HYVjcXyBkRWPai9E",
-  authDomain: "service-auth-e9e4e.firebaseapp.com",
-  databaseURL: "https://service-auth-e9e4e.firebaseio.com",
-  projectId: "service-auth-e9e4e",
-  storageBucket: "service-auth-e9e4e.appspot.com",
-  messagingSenderId: "667231808245"
+  apiKey: "AIzaSyCOMuidIz53S_1_iW5isfoiy3J66QVp720",
+  authDomain: "dsc-covid-19.firebaseapp.com",
+  databaseURL: "https://dsc-covid-19.firebaseio.com",
+  projectId: "dsc-covid-19",
+  storageBucket: "dsc-covid-19.appspot.com",
+  messagingSenderId: "724650717987",
+  appId: "1:724650717987:web:1e96e61a05f00140a35296",
+  measurementId: "G-LJRVG3223S"
 });
+
+export const firestore = app.firestore();
+export const storage = app.storage();
+export const auth = app.auth();
 
 export default app;
 
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCOMuidIz53S_1_iW5isfoiy3J66QVp720",
-//   authDomain: "dsc-covid-19.firebaseapp.com",
-//   databaseURL: "https://dsc-covid-19.firebaseio.com",
-//   projectId: "dsc-covid-19",
-//   storageBucket: "dsc-covid-19.appspot.com",
-//   messagingSenderId: "724650717987",
-//   appId: "1:724650717987:web:1e96e61a05f00140a35296",
-//   measurementId: "G-LJRVG3223S"
-// };
 
-// export const firebase = firebaseInit.initializeApp(firebaseConfig);
-// export const firestore = firebase.firestore();
-// export const storage = firebase.storage();
-// export const auth = firebase.auth();
+
