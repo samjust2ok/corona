e5c6cf1d1689e39f432f01872ac8e3ab35ab4a4a
joshtom <olajidejoshua4real@gmail.com>
2020-03-25 10:01:44 +0100

    Create component option

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormLabel from '@material-ui/core/FormLabel';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -9,6 +18,14 @@ const useStyles = makeStyles(theme => ({
       marginBottom: theme.spacing(1),
       width: '100%',
     },
+  },
+  formControl: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+    minWidth: '100%',
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
   },
 }));
 
@@ -31,4 +48,83 @@ export default function Input(props) {
       />
     </div>
   );
+}
+
+export function Gender(props)  {
+  const classes = useStyles();
+  const [gender, setGender] = React.useState('');
+
+  const handleChange = event => {
+    setGender(event.target.value);
+  };
+  return (
+    <div className={classes.root}>
+    <FormControl variant="outlined" className={classes.formControl}>
+        <InputLabel id="demo-simple-select-outlined-label">{props.label}</InputLabel>
+        <Select
+          labelId="demo-simple-select-outlined-label"
+          id="demo-simple-select-outlined"
+          value={gender}
+          onChange={handleChange}
+          label={props.label}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={props.value1}>{props.value1Text}</MenuItem>
+          <MenuItem value={props.value2}>{props.value1Text}</MenuItem>
+        </Select>
+      </FormControl>
+      </div>
+  )
+}
+
+export function Option2(props) {
+  return (
+    <FormControl component="fieldset">
+    <FormLabel component="legend">{props.text}</FormLabel>
+      <RadioGroup row aria-label="position" name="position" defaultValue="top">
+     <FormControlLabel
+          value={props.value}
+          control={<Radio color="secondary" />}
+          label={props.label}
+          labelPlacement="end"
+        />
+        <FormControlLabel 
+          value={props.value_2}
+          control={<Radio color="secondary" />}
+          label={props.label_2}
+          labelPlacement="end"
+        />
+      </RadioGroup>
+    </FormControl>
+  )
+}
+
+export function Option3(props) {
+  return (
+    <FormControl component="fieldset">
+    <FormLabel component="legend">{props.text}</FormLabel>
+      <RadioGroup row aria-label="position" name="position" defaultValue="top">
+     <FormControlLabel
+          value={props.value}
+          control={<Radio color="secondary" />}
+          label={props.label}
+          labelPlacement="end"
+        />
+        <FormControlLabel 
+          value={props.value_2}
+          control={<Radio color="secondary" />}
+          label={props.label_2}
+          labelPlacement="end"
+        />
+        <FormControlLabel 
+          value={props.value_3}
+          control={<Radio color="secondary" />}
+          label={props.label_3}
+          labelPlacement="end"
+        />
+      </RadioGroup>
+    </FormControl>
+  )
 }
