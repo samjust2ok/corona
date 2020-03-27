22ab9521f420c53ab5ac0ed9f590c7620dd4f785
joshtom <olajidejoshua4real@gmail.com>
2020-03-27 18:18:03 +0100

    Create Cases file and  object to include all Cases info

---
+++ src/Cases.js
@@ -0,0 +1,29 @@
+const Cases = [
+    {
+        name: 'Lagos',
+        isCases: '12',
+        isPositive: '10',
+        isNegative: '0',
+        isRecovered: '7',
+        isDeath: '2'
+    },
+  
+    {
+        name: 'Ibadan',
+        isCases: '2',
+        isPositive: '1',
+        isNegative: '0',
+        isRecovered: '0',
+        isDeath: '0'
+    },
+    {
+      name: 'Saki',
+      isCases: '0',
+      isPositive: '0',
+      isNegative: '0',
+      isRecovered: '0',
+      isDeath: '0'
+  },
+  ]
+
+export default Cases;
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import './styles.css';
 
-const Card = () => (
+const Card = (props) => (
   <div className='card-container'>
-    <h4>Lagos</h4>
+    <h4>{props.name}</h4>
     <ul>
       <li className='card-row'>
         <p>Total Cases</p>
-        <p>12</p>
+        <p>{props.isCases}</p>
       </li>
       <li className='card-row'>
         <p>Positive</p>
-        <p>12</p>
+        <p>{props.isPositive}</p>
       </li>
       <li className='card-row'>
         <p>Negative</p>
-        <p>12</p>
+        <p>{props.isNegative}</p>
       </li>
       <li className='card-row'>
         <p>Recovered</p>
-        <p>12</p>
+        <p>{props.isRecovered}</p>
       </li>
       <li className='card-row'>
         <p>Death</p>
-        <p>12</p>
+        <p>{props.isDeath}</p>
       </li>
     </ul>
   </div>
