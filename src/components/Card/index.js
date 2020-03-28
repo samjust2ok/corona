03571e5808c7886e39c0dfef8414bc7fadd6190e
joshtom <olajidejoshua4real@gmail.com>
2020-03-28 06:51:22 +0100

    Update card component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,28 +1,36 @@
 import React from 'react';
 import './styles.css';
+import RoomIcon from '@material-ui/icons/Room';
+import Divider from '@material-ui/core/Divider';
 
 const Card = (props) => (
   <div className='card-container'>
-    <h4>{props.name}</h4>
+     <h4> <RoomIcon /> {props.name}</h4>
+     <span className="underline"> </span>
+     
     <ul>
       <li className='card-row'>
-        <p>Total Cases</p>
+        <p className="tc">Total Cases</p>
         <p>{props.isCases}</p>
       </li>
+      <Divider />
       <li className='card-row'>
-        <p>Positive</p>
+        <p className="p">Positive</p>
         <p>{props.isPositive}</p>
       </li>
+      <Divider />
       <li className='card-row'>
-        <p>Negative</p>
+        <p className="n">Negative</p>
         <p>{props.isNegative}</p>
       </li>
+      <Divider />
       <li className='card-row'>
-        <p>Recovered</p>
+        <p className="r">Recovered</p>
         <p>{props.isRecovered}</p>
       </li>
+      <Divider />
       <li className='card-row'>
-        <p>Death</p>
+        <p className="d">Death</p>
         <p>{props.isDeath}</p>
       </li>
     </ul>
